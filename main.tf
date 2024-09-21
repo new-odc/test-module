e2c3b08ce92f4d7d61c50e6d84c123d405a4057f
Ravikanttyagi <ravikanttyagi@gmail.com>
2024-09-21 17:20:23 +0100

    feat: Added new config

--- main.tf
+++ main.tf
@@ -1,84 +1,38 @@
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-module "project-factory" {
-  source = "./modules/core_project_factory"
-
-  group_email                        = module.gsuite_group.email
-  group_role                         = var.group_role
-  lien                               = var.lien
-  manage_group                       = var.group_name != "" ? true : false
-  random_project_id                  = var.random_project_id
-  random_project_id_length           = var.random_project_id_length
-  org_id                             = var.folder_id != "" ? null : var.org_id
-  name                               = var.name
-  project_id                         = var.project_id
-  shared_vpc                         = var.svpc_host_project_id
-  enable_shared_vpc_service_project  = var.svpc_host_project_id != ""
-  enable_shared_vpc_host_project     = var.enable_shared_vpc_host_project
-  grant_network_role                 = var.grant_network_role
-  billing_account                    = var.billing_account
-  folder_id                          = var.folder_id
-  create_project_sa                  = var.create_project_sa
-  project_sa_name                    = var.project_sa_name
-  sa_role                            = var.sa_role
-  activate_apis                      = var.activate_apis
-  activate_api_identities            = var.activate_api_identities
-  usage_bucket_name                  = var.usage_bucket_name
-  usage_bucket_prefix                = var.usage_bucket_prefix
-  shared_vpc_subnets                 = var.shared_vpc_subnets
-  labels                             = var.labels
-  bucket_project                     = var.bucket_project
-  bucket_name                        = var.bucket_name
-  bucket_location                    = var.bucket_location
-  bucket_versioning                  = var.bucket_versioning
-  bucket_labels                      = var.bucket_labels
-  bucket_force_destroy               = var.bucket_force_destroy
-  bucket_ula                         = var.bucket_ula
-  bucket_pap                         = var.bucket_pap
-  auto_create_network                = var.auto_create_network
-  disable_services_on_destroy        = var.disable_services_on_destroy
-  default_service_account            = var.default_service_account
-  disable_dependent_services         = var.disable_dependent_services
-  vpc_service_control_attach_enabled = var.vpc_service_control_attach_enabled
-  vpc_service_control_attach_dry_run = var.vpc_service_control_attach_dry_run
-  vpc_service_control_perimeter_name = var.vpc_service_control_perimeter_name
-  vpc_service_control_sleep_duration = var.vpc_service_control_sleep_duration
-  default_network_tier               = var.default_network_tier
-  tag_binding_values                 = var.tag_binding_values
-  cloud_armor_tier                   = var.cloud_armor_tier
-  deletion_policy                    = var.deletion_policy
+provider "google" {
+  credentials = file(var.credentials_file_path)  # Path to service account key
+  region      = var.region
 }
 
-/******************************************
-  Setting API service accounts for shared VPC
- *****************************************/
-module "shared_vpc_access" {
-  source                             = "./modules/shared_vpc_access"
-  enable_shared_vpc_service_project  = var.svpc_host_project_id != "" ? true : false
-  host_project_id                    = var.svpc_host_project_id
-  service_project_id                 = module.project-factory.project_id
-  active_apis                        = var.activate_apis
-  shared_vpc_subnets                 = var.shared_vpc_subnets
-  service_project_number             = module.project-factory.project_number
-  lookup_project_numbers             = false
-  grant_services_security_admin_role = var.grant_services_security_admin_role
-  grant_network_role                 = var.grant_network_role
-  # Workaround for import complaining about count cannot determine resource instances
-  # until apply. https://github.com/hashicorp/terraform/issues/24690
-  depends_on = [module.project-factory.enabled_apis]
+# Generate a random project ID using epoch time
+locals {
+  epoch_time = floor(timeadd(timestamp(), 0) / 1000)
+  project_id = "${var.project_name}-${local.epoch_time}"
+}
+
+resource "google_project" "project" {
+  name       = var.project_name
+  project_id = local.project_id  # Use the generated project ID
+  org_id     = var.organization_id
+  folder_id  = var.folder_id
+}
+
+resource "google_project_service" "services" {
+  for_each = toset(var.activate_apis)
+  project  = google_project.project.project_id
+  service  = each.value
+}
+
+resource "google_compute_shared_vpc_service_project" "service_project" {
+  project     = google_project.project.project_id
+  host_project = var.svpc_host_project_id
+}
+
+resource "google_compute_subnetwork_iam_member" "subnet_roles" {
+  for_each = toset(var.shared_vpc_subnets)
+  subnetwork = each.value
+  project    = var.svpc_host_project_id
+  region     = var.region
+  role       = "roles/compute.networkUser"
+  member     = "serviceAccount:${google_project.project.project_id}@${google_project.project.project_id}.iam.gserviceaccount.com"
 }
 
